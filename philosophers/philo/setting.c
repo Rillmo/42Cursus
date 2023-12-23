ec36ad6d9265bf6cea2b13f4cd180c729def5c39
Junho Kim <maxman306@gmail.com>
2023-12-23 16:22:56 +0900

    simple multi-thread test

--- philosophers/philo/setting.c
+++ philosophers/philo/setting.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   setting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: junkim2 <junkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/22 19:06:05 by junkim2           #+#    #+#             */
-/*   Updated: 2023/12/22 22:20:47 by junkim2          ###   ########.fr       */
+/*   Created: 2023/12/23 15:54:00 by junkim2           #+#    #+#             */
+/*   Updated: 2023/12/23 16:07:13 by junkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,20 @@ void	set_info(t_info *info, int argc, char **argv)
 	info->time_to_eat = ft_atoi(argv[3]);
 	info->time_to_sleep = ft_atoi(argv[4]);
 	if (argc > 5)
-		info->num_of_times_each_philo_must_eat = ft_atoi(argv[5]);
+		info->num_of_eat = ft_atoi(argv[5]);
 }
 
-void	*phlio_do_something(void *arg)
+void	set_philo(t_info *info)
 {
-	(void)arg;
-	printf("I'am 신뢰에요\n");
-	return (NULL);
-}
+	int	i;
 
-int	main(int argc, char **argv)
-{
-	t_info		info;
-	pthread_t	phlio;
-
-	set_info(&info, argc, argv);
-	pthread_create(&phlio, NULL, &phlio_do_something, NULL);
-	// pthread_detach(phlio);
+	info->philos = (t_philo *)malloc(sizeof(t_philo) * info->num_of_philo);
+	if (info->philos == NULL)
+		return ;
+	i = 0;
+	while (i < info->num_of_philo)
+	{
+		info->philos[i].num = i + 1;
+		i++;
+	}
 }
