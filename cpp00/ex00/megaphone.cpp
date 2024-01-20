19107d0514589795b39db0dd2bfe6a5f03ed9b9b
Kirby <maxman306@gmail.com>
2024-01-20 18:26:27 +0900

    Finished ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+std::string	strToUpper(std::string str) {
+	int	len = str.length();
+
+	for (int i=0; i<len; i++) {
+		if ('a' <= str[i] && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	return (str);
+}
+
+int main(int argc, char *argv[]) {
+	std::string str;
+
+	if (argc == 1)
+		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else {
+		for (int i=1; i<argc; i++) {
+			str.append(strToUpper(argv[i]));
+		}
+	}
+	std::cout << str << std::endl;
+	return 0;
+}
