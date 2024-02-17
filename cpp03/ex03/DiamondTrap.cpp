b73ced5b981408a60017f3a2e1608c14d27a2970
Kirby <maxman306@gmail.com>
2024-02-17 12:23:38 +0900

    work in progress..

---
+++ cpp03/ex03/DiamondTrap.cpp
@@ -0,0 +1,21 @@
+#include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap() :ClapTrap("DEFAULT_clap_trap") {
+	this->_name = "DEFAULT";
+}
+
+DiamondTrap::DiamondTrap(std::string name) {
+
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap& obj) {
+
+}
+
+DiamondTrap& DiamondTrap::operator=(const DiamondTrap& obj) {
+
+}
+
+DiamondTrap::~DiamondTrap() {
+
+}
---
+++ cpp03/ex03/DiamondTrap.hpp
@@ -0,0 +1,18 @@
+#ifndef DIAMONDTRAP_H
+#define DIAMONDTRAP_H
+
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+
+class DiamondTrap : public FragTrap, public ScavTrap {
+	private:
+		std::string _name;
+	public:
+		DiamondTrap();
+		DiamondTrap(std::string name);
+		DiamondTrap(const DiamondTrap& obj);
+		DiamondTrap& operator=(const DiamondTrap& obj);
+		~DiamondTrap();
+};
+
+#endif
