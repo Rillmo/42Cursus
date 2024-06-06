c96e244689ab7c432069c52cba7e2f084d3b0e62
Kirby <maxman306@gmail.com>
2024-06-06 15:25:41 +0900

    feat: finished ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,11 +1,13 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <stdexcept>
+
 template<typename T>
 class Array {
 	private:
 		T* _arr;
-		unsigned int size;
+		std::size_t _size;
 	public:
 		Array() {
 			_arr = new T();
@@ -13,23 +15,43 @@ class Array {
 		};
 
 		Array(const Array& arr) {
-			for (int i=0; i<size(); i++)
+			delete _arr;
+			_arr = new T[arr.size()];
+			for (std::size_t i=0; i<size(); i++)
 				_arr[i] = arr[i];
+			_size = arr.size();
 		};
 
 		Array& operator=(const Array& arr) {
-			for (int i=0; i<size(); i++)
+			delete _arr;
+			_arr = new T[arr.size()];
+			for (std::size_t i=0; i<size(); i++)
 				_arr[i] = arr[i];
+			_size = arr.size();
 			return *this;
 		};
 
-		Array(unsigned int n) {
+		Array(std::size_t n) {
+			T* tmp = new T();
 			_arr = new T[n];
+			for (std::size_t i=0; i<n; i++)
+				_arr[i] = *tmp;
 			_size = n;
+			delete tmp;
 		};
 
-		unsigned int size() const {
+		~Array() {
+			delete _arr;
+		}
+
+		std::size_t size() const {
 			return _size;
+		};
+
+		T& operator[](std::size_t idx) const {
+			if (idx >= _size)
+				throw std::out_of_range("OUT OF BOUND");
+			return _arr[idx];
 		}
 };
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,9 +1,30 @@
 #include "Array.hpp"
+#include <iostream>
 
 int main() {
-	Array<int> arr;
+	Array<int> arr1;
+	Array<int> arr2(10);
 
-	for (int i=0; i<arr->size(); i++) {
-		
+	try {
+		std::cout << "arr1: ";
+		for (unsigned int i=0; i<arr1.size(); i++) {
+			std::cout << arr1[i] << " ";
+		}
+		std::cout << std::endl;
+
+		std::cout << "arr2: ";
+		for (unsigned int i=0; i<arr2.size(); i++) {
+			std::cout << arr2[i] << " ";
+		}
+		std::cout << std::endl;
+
+		std::cout << "arr1: ";
+		arr1 = arr2;
+		for (unsigned int i=0; i<arr1.size(); i++) {
+			std::cout << arr1[i] << " ";
+		}
+		std::cout << std::endl;
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
 	}
 }
