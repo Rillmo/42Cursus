79a31afe652d675cdfbdbc473a0f53e19ee50cda
Kirby <maxman306@gmail.com>
2024-06-15 23:54:25 +0900

    feat: really finished! added Readme.md also:

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -85,14 +85,6 @@ std::vector<int> getJacobsthalNumber(int n) {
 	return res;
 }
 
-void displayPairVector(std::vector< std::pair<long, long> > pvec) {
-	std::vector< std::pair<long, long> >::iterator it;
-
-	for (it=pvec.begin(); it!=pvec.end(); it++)
-		std::cout << "(" << it->first << "," << it->second << ") ";
-	std::cout << std::endl;
-}
-
 void setIdxpairs(std::vector<int> log, std::vector< std::pair<long,long> >& idxpairs) {
 	std::size_t i;
 
@@ -102,8 +94,6 @@ void setIdxpairs(std::vector<int> log, std::vector< std::pair<long,long> >& idxp
 		else
 			idxpairs.push_back(std::make_pair(log[i], log[i+1]));
 	}
-	std::cout << "idxpairs";
-	displayPairVector(idxpairs);
 }
 
 std::size_t binarySearch(std::vector< std::pair<long,long> > pairs, std::size_t low, std::size_t high, int target) {
@@ -134,7 +124,6 @@ void PmergeMe::binaryInsertion(std::vector< std::pair<long,long> > pairs, std::v
 	std::size_t idx;
 	std::vector< std::pair<long,long> > idxpairs;
 
-	std::cout << "binaryInsertion" << std::endl;
 	// 1. log를 기준으로 inxpairs를 세팅
 	setIdxpairs(log, idxpairs);
 	lastInsert = 0;
@@ -152,12 +141,9 @@ void PmergeMe::binaryInsertion(std::vector< std::pair<long,long> > pairs, std::v
 				continue;
 			target = pairs[k-1].second;
 			idx = binarySearch(pairs, 0, k-1, target);
-			std::cout << "target:" << target << " range(0," << k-1 << ") idx:" << idx << std::endl;
 			pairs[k-1].second = -1;
 			pairs.insert(pairs.begin()+idx, std::make_pair(target, -1));
 			idxpairs.insert(idxpairs.begin()+idx, std::make_pair(idxpairs[k-1].second, -1));
-			displayPairVector(pairs);
-			displayPairVector(idxpairs);
 		}
 		lastInsert = j;
 		i++;
@@ -174,46 +160,7 @@ void PmergeMe::binaryInsertion(std::vector< std::pair<long,long> > pairs, std::v
 		if (pairs[i].first < INT_MAX)
 			mainchain.push_back(pairs[i].first);
 	}
-	std::cout << "==========\n";
 }
-
-/*
-	1. pair로 쪼갠다.
-		0  1  2  3  4  5		0  1  2
-		59 69 74 77 43 92	->	59 74 43
-								69 77 92
-	2. 재귀 레이어를 내려가며 짝이 1개일때까지 쪼갠다.
-		2-1. 전체 수열의 log를 만든다.
-			0  1  2			0  1  2
-			59 74 43	->  0  2  4
-			69 77 92		1  3  5
-		2-2. 각 pair들의 내부를 내림차순 swap한다.
-			0  1  2
-			69 77 92	
-			59 74 43   
-		2-3. 1개일때까지 2-1~2-2 반복
-			0  1  2			0  1  2			0	1
-			69 77 92  -> 	77 69 92   ->	77	X
-			59 74 43		74 59 43		69 	92
-			0  1  2			1  0  2
-	3. 쪼개진 짝을 이진삽입으로 다시 합친다. ([idx순서배열]과 [pair] 필요)
-		3-1. 이때 pair는 아래와 같다.
-			0  1  2
-			69 77 92
-			59 74 43
-		3-2. 이 pair들을 이진삽입한다.
-			0  1  2			0  1  2  3			0  1  2  3  4			0  1  2  3  4  5
-			69 77 92   ->	59 69 77 92   ->	43 59 69 77 92   -> 	43 59 69 74 77 92
-	      	59 74 43		X  59 74 43			X  X  59 74 43			X  X  59 X  74 43
-		3-3. 이진삽입과 동시에 정렬되는 인덱스의 변화를 기준으로 [idx순서배열]도 바꾼다.
-			0  1  2	 3  4  5		0  1  2			0  1  2  3		0  1  2  3  4		0  1  2  3  4  5
-			1  0  3  2  5  4   ->	1  3  5   -> 	0  1  3  5	->	4  0  1  3  5	->	4  0  1  2  3  5
-									0  2  4			X  0  2  4		X  X  0  2  4		X  X  0  X  2  4
-		3-4. 완성된 pair에서 mainchain 추출해 _vmainchain에 저장하고, [idx순서배열]도 _vlog에 저장한다.
-			_vmainchain :	43 59 69 74 77 92
-			_vlog : 		4  0  1  2  3  5
-	4. _vlog로 기존의 pair를 재배치한다.
-*/
 
 /* VECTOR */
 void PmergeMe::sort(std::vector<int>& vec) {
@@ -235,8 +182,6 @@ void PmergeMe::sort(std::vector<int>& vec) {
 	for (i=0; i<vec.size(); i++) {
 		log.push_back(i);
 	}
-	// std::cout << "log ";
-	// display(log);
 
 	// 2-2. 각 pair들의 내부 요소를 내림차순 swap한다.
 	for (std::size_t j=0, i=0; i<pairs.size(); i++, j+=2) {
@@ -245,15 +190,12 @@ void PmergeMe::sort(std::vector<int>& vec) {
 			std::swap(log[j], log[j+1]);
 		}
 	}
-	std::cout << "log ";
-	display(log);
 
 	// 3. 각 pair들의 첫번째 요소들을 기준으로 pair들을 오름차순 정렬한다. (재귀)
 	for (i=0; i<pairs.size(); i++) {
 		if (pairs[i].first <= INT_MAX)
 			mainchain.push_back(pairs[i].first);
 	}
-	displayPairVector(pairs);
 	if (mainchain.size() > 1)
 		sort(mainchain);
 	// 하위 deps에서 저장했던 _vmainchain과 _vlog를 가져온다.
@@ -264,25 +206,16 @@ void PmergeMe::sort(std::vector<int>& vec) {
 	if (_vlog.size() != 0) {
 		tmpPairs = pairs;
 		pairs.clear();
-		std::cout << "_vlog: ";
-		display(_vlog);
-		std::cout << "tmpPairs: ";
-		displayPairVector(tmpPairs);
 		for (i=0; i<_vlog.size(); i++) {
 			pairs.push_back(tmpPairs[_vlog[i]]);
 		}
-		std::cout << "주목! ";
-		displayPairVector(pairs);
 	}
 	// 3-1. 이제 이진탐색을 기반으로 삽입정렬을 수행한다.
 	binaryInsertion(pairs, log, mainchain);
 	// 3-2. 삽입정렬이 끝나면, mainchin을 _vmainchain에 저장한다.
-	std::cout << "3-1: ";
-	display(log);
-	display(mainchain);
 	_vmainchain = mainchain;
+	// 3-3. 정렬된 log도 _vlog에 저장한다.
 	_vlog = log;
-	display(_vlog);
 	_vec = _vmainchain;
 }
 
